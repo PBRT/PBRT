10c7c2cbe2d249ca592505c0cf26e2f6f9db1692
PBRT <pierre.beard@workangel.com>
2015-10-16 10:14:43 +0100

    Add port

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/mail', function(req, res) {
   }
 });
 
-var server = app.listen(9000, function () {
+var server = app.listen((process.env.PORT || 8080), function () {
 
   var host = server.address().address;
   var port = server.address().port;
