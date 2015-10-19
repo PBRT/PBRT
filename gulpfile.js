d6796a26c32c99582677b6e5671e0d3218050552
PBRT <pierre.beard@workangel.com>
2015-10-19 08:57:23 +0100

    Handle boostrap config

--- gulpfile.js
+++ gulpfile.js
@@ -17,23 +17,38 @@ gulp.task('webpack', function(callback) {
   });
 });
 
+// Copy server files
 gulp.task('copy:server', ['clean'], function() {
   gulp.src([
     'server/**',
   ]).pipe(gulp.dest('dist/server'));
 });
 
+// Copy package.json
 gulp.task('copy:package-json', ['clean'], function() {
   gulp.src([
     'package.json',
   ]).pipe(gulp.dest('dist/'));
 });
 
+// Clean server folder
 gulp.task('clean', function() {
   return del(['dist/**']);
 });
 
+// Delete auto generate bootstrap file
+gulp.task('clean:bootstrap', function() {
+  return del(['node_modules/bootstrap-webpack/bootstrap.config.js']);
+});
+// Copy bootstrap file
+gulp.task('copy:bootstrap', ['clean:bootstrap'], function() {
+  gulp.src([
+    'bootstrap.config.js',
+  ]).pipe(gulp.dest('node_modules/bootstrap-webpack/'));
+});
+
 gulp.task('build', [
+  'copy:bootstrap',
   'copy:server',
   'copy:package-json',
   'webpack',
