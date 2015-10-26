1c4ebed16a22ea858234a2a6cef3a38132b74627
PBRT <pierre.beard@workangel.com>
2015-10-26 18:37:01 +0000

    Lasts ones

--- client/modules/experience/experience.constant.js
+++ client/modules/experience/experience.constant.js
@@ -1,7 +1,8 @@
 module.exports = {
   experiences: [
     {
-      title: 'WorkAngel Ltd (10/2014 - Current)',
+      title: 'WorkAngel Ltd',
+      date: '(10/2014 - Current)',
       subtitle:
         'Building a social recognition platform for improving employees benefits. ' +
         'This includes responsive web application and CMS. My responsabilities are :' +
@@ -12,7 +13,8 @@ module.exports = {
       icon: 'touch.png',
     },
     {
-      title: 'INFOTEL CONSEIL (02/2014 - 08/2014)',
+      title: 'Infotel Conseil',
+      date: '(02/2014 - 08/2014)',
       subtitle:
         'Build a responsive cross platform web application responsive used for Sport Events. ' +
         'I developed specifications, carried out technology analysis, ' +
@@ -22,7 +24,8 @@ module.exports = {
       icon: 'run.png',
     },
     {
-      title: 'M.S. Computer Science (2009 - 2014)',
+      title: 'M.S. Computer Science',
+      date: '(2009 - 2014)',
       subtitle: 'Graduated from the I.N.S.A. of Toulouse in Computer Science (engineering school). ' +
       'Learnt software development, physics, embedded systems, computer science and digital circuits.',
       duration: '5 years',
@@ -30,7 +33,8 @@ module.exports = {
       icon: 'college.png',
     },
     {
-      title: 'AERODRONES (06/2013 - 09/2013)',
+      title: 'Aerodrones',
+      date: '(06/2013 - 09/2013)',
       subtitle:
         'Conception and development of and embedded software driving the orientation of a camera fixed on a drone.',
       duration: '4 months',
